717c8e92fe4ab749ab1622686af2fdfa505f92e1
David de Boer <david@ddeboer.nl>
2016-08-03 11:27:05 +0200

    Add Erlang

---
+++ mod_ginger_template.erl
@@ -0,0 +1,13 @@
+%% @doc This module ...
+-module(mod_ginger_template).
+-author("Driebit <tech@driebit.nl>").
+
+-mod_title("Ginger Template Module").
+-mod_description("Helps you set up a new Ginger module quickly").
+-mod_prio(500).
+
+-export([
+    %% export your functions here
+]).
+
+-include("zotonic.hrl").
